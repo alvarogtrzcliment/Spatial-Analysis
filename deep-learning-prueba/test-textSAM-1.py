40e534bbc76d9b480d16fab13342dfdbc4771ba4
Álvaro Gutiérrez <alvarogtrzcliment@gmail.com>
2024-04-03 18:07:19 +0200

    Prueba M-40

--- deep-learning-prueba/test-textSAM-1.py
+++ deep-learning-prueba/test-textSAM-1.py
@@ -2,9 +2,6 @@
 
 from arcgis.gis import GIS
 import pandas as pd
-from PIL import Image
-import requests
-from io import BytesIO
 import os
 import arcpy
 from arcpy.ia import *
@@ -15,18 +12,10 @@ from datetime import datetime
 
 gis = GIS("home")
 
-# Accedo al servicio
-
-idServicio = "13c0065c40984c34baae3493061646ce"
-
-camarasTrafico = gis.content.get(idServicio).layers[0]
-
-camarasTraficoDF = camarasTrafico.query(as_df=True)
-
 # ¡¡IMPORTANTE INSERTA LA RUTA DEL DIRECTORIO DE TRABAJO!!ç
 # Por favor crea una nueva carpeta donde se guarde la ejecución
 
-directorioTrabajo = ''
+directorioTrabajo = 'C:\\Users\\alvaro.gutierrez\\DatosLocal\\Prueba3Camaras'
 
 # Check de las extensiones necesarias
 
@@ -49,19 +38,9 @@ arcpy.env.workspace = rutaGDB
 
 print('GDB Creada')
 
-# Crear la Carpeta donde se guardan las imagenes
-
-nombreCarpetaImagenes = 'ImagenesEvaluadas'
-
-rutaCarpetaImagenes = os.path.join(directorioTrabajo,nombreCarpetaImagenes)
-
-os.mkdir(rutaCarpetaImagenes)
-
-print('Directorio Imagenes Creado')
-
 # INserta Ruta del TextSAM.dlpk
 
-textSAM_dlpk = ""
+textSAM_dlpk = "C:/Users/alvaro.gutierrez/DatosLocal/Imagenes-deep-learning/DeepLearningPackage/TextSAM.dlpk"
 
 
 # Configuración de la herramienta
@@ -91,19 +70,11 @@ dFSalida = pd.DataFrame(columns = ['FID Image', 'VehiclesCount', 'Date'])
 
 # Configuración del Bucle que recorre todos los elementos
   
-for FID in range(len(camarasTraficoDF)):
-
-  print(f'Iteración nº: {FID}')
-
-  # LLamada al Servicio
-  
-  response = requests.get(camarasTraficoDF.iloc[FID,6])
+for FID in range(42):
 
   # Abro la imagen y la guardo
 
-  responseImage = Image.open(BytesIO(response.content))
-
-  responseImage.save(os.path.join(rutaCarpetaImagenes,f'imagenDGT{FID}.jpg'))
+  rutaCarpetaImagenes = os.path.join(directorioTrabajo,'Imagenes')
 
   urlIm = os.path.join(rutaCarpetaImagenes,f'imagenDGT{FID}.jpg')
 
@@ -118,6 +89,8 @@ for FID in range(len(camarasTraficoDF)):
   DetectDGT(urlIm, layerOutputUrl,'car, vehicle')
 
   numeroVehiculos = arcpy.GetCount_management(layerName).getOutput(0)
+
+  print(f'Iteración nº: {FID} Número de vehículos: {numeroVehiculos}')
 
   dFSalida.loc[len(dFSalida.index)] = [f'imagenDGT{FID}', float(numeroVehiculos), datetime.now()]
 
